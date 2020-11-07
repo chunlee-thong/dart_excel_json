31298fb6c9b2e7b0c879071dd7d68cb33b8ae1a1
chunleethong <thong.chunlee@optistech.com>
2020-11-07 10:45:53 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,14 +1,22 @@
 import 'dart:collection';
-import 'dart:io';
-import 'package:excel/excel.dart';
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:excel/excel.dart';
 
 String one_language_name;
-final String json_path = "./generated";
-final String locale_key_class_path = "./generated";
+
+//Required
+final String saved_json_path = "";
+
+//Required
+final String saved_locale_key_class_path = "";
+
+//Required
+final String excel_file_path = "";
+
 void main(List<String> args) async {
-  final file = "./translation_example.xlsx";
-  final bytes = File(file).readAsBytesSync();
+  final bytes = File(excel_file_path).readAsBytesSync();
   final excel = Excel.decodeBytes(bytes);
   final sheetName = "Translation";
   final sheet = excel.tables[sheetName];
@@ -19,11 +27,18 @@ void main(List<String> args) async {
 }
 
 Future<void> generateJSONFile(Sheet sheet) async {
+  //get language count by column count minus 1 (minus 1 becuase first column is a key column)
   int langage_count = sheet.maxCols - 1;
+
+  //get key count by row count minus 1 (minus 1 because first row is a title row)
   int key_count = sheet.maxRows - 1;
-  final language_list = List<int>.generate(langage_count, (i) => i + 1);
+
+  //getenrate language list
+  List<int> language_list = List<int>.generate(langage_count, (i) => i + 1);
   for (var lang in language_list) {
+    //SplayTreeMap auto sort it's key
     SplayTreeMap<String, dynamic> data = SplayTreeMap<String, dynamic>();
+
     var key_list = List<int>.generate(key_count, (i) => i + 1);
     for (var key_index in key_list) {
       String key = sheet
@@ -51,16 +66,20 @@ Future<void> generateJSONFile(Sheet sheet) async {
         ))
         .value
         .toString();
+
+    //Save file language name to access json file and read key for LocaleKeys class
     one_language_name = language_name;
+
+    //
     data.keys.toList()..sort();
     String json_data = json.encode(data);
-    final language_file = File("$json_path/${language_name}.json");
+    final language_file = File("$saved_json_path/${language_name}.json");
     await language_file.writeAsString(json_data);
   }
 }
 
 void generateDartClass() async {
-  File json_file = File("$json_path/${one_language_name}.json");
+  File json_file = File("$saved_json_path/${one_language_name}.json");
   String json_data = await json_file.readAsString();
   Map<String, dynamic> map_data = json.decode(json_data);
   String dartClass = "class LocaleKeys {\n";
@@ -75,7 +94,7 @@ void generateDartClass() async {
 
   dartClass += "}";
 
-  File dartClassFile = File("$locale_key_class_path/locale_keys.dart");
+  File dartClassFile = File("$saved_locale_key_class_path/locale_keys.dart");
   dartClassFile.writeAsString(dartClass);
 }
 
